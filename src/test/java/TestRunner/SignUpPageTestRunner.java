9331b7ee6f71ad7f3afdf786374b942b03dd5db4
Jamil Kawsher <jamilkawsher@gmail.com>
2023-04-19 04:11:09 +0600

    Email verification Part Added

--- src/test/java/TestRunner/SignUpPageTestRunner.java
+++ src/test/java/TestRunner/SignUpPageTestRunner.java
@@ -19,11 +19,17 @@ public class SignUpPageTestRunner extends Setup {
         ((JavascriptExecutor) driver).executeScript("window.open()");
         driver.switchTo().window((String) driver.getWindowHandles().toArray()[1]);
 
-        driver.get("https://temp-mail.org/en/");
+        driver.get("https://mailpoof.com/");
+        String name=faker.name().firstName();
         Thread.sleep(5000);
-        WebElement emailElement = driver.findElement(By.id("mail"));
-        Thread.sleep(3000);
-        String email = emailElement.getAttribute("value");
+        WebElement emailElement = driver.findElement(By.tagName("input"));
+        emailElement.clear();
+        Thread.sleep(1000);
+        emailElement.sendKeys(name+"00777");
+        Thread.sleep(1000);
+        driver.findElements(By.tagName("button")).get(3).click();
+        Thread.sleep(1000);
+        String email = name+"00777"+"@explodemail.com";
         return email;
 
     }
@@ -46,8 +52,18 @@ public class SignUpPageTestRunner extends Setup {
         String password = "Eb9C*f\"A2om]nP2";
         String category = "Other";
         homePage.signUp(firstName, lastName, email, businessName, password, category);
+        Thread.sleep(4000);
+        driver.switchTo().window((String) driver.getWindowHandles().toArray()[1]);
         Thread.sleep(1000);
-        homePage.otp.sendKeys("123456");
+        driver.findElement(By.className("p-5")).click();
+        Thread.sleep(2000);
+     String verificationText=   driver.findElements(By.tagName("td")).get(6).getText();
+
+        String regex = "[^0-9]+";
+        String verificationCode = verificationText.replaceAll(regex, "");
+        driver.switchTo().window((String) driver.getWindowHandles().toArray()[0]);
+        Thread.sleep(2000);
+        homePage.otp.sendKeys(verificationCode);
         Thread.sleep(1000);
     }
 }
